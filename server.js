52655530844c071a2d65ca9faf3f02837e3e26a9
xinrantaoangela <xinran.tao2001@gmail.com>
2024-06-05 20:23:02 +0800

    beckend setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req,res){
+    res.send("Express run here")
+})
+app.listen(3001,function(){
+    console.log("express server is running on port 3001")
+})
